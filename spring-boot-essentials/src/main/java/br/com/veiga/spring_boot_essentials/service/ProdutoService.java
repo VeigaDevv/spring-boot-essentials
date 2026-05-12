3a843ad7bed6277741fbcbe612d79c96fd7d4206
VeigaDevv <veigadevv@gmail.com>
2026-05-12 09:57:00 -0700

    refactor: improve product initialization and implement dynamic ID generation

--- spring-boot-essentials/src/main/java/br/com/veiga/spring_boot_essentials/service/ProdutoService.java
+++ spring-boot-essentials/src/main/java/br/com/veiga/spring_boot_essentials/service/ProdutoService.java
@@ -1,6 +1,7 @@
 package br.com.veiga.spring_boot_essentials.service;
 
 import br.com.veiga.spring_boot_essentials.database.model.ProdutoEntity;
+import br.com.veiga.spring_boot_essentials.dto.ProdutoDTO;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -9,28 +10,51 @@ import java.util.List;
 @Service
 public class ProdutoService {
 
-    private static final List<ProdutoEntity> PRODUTOS = List.of(
-            ProdutoEntity.builder()
-                    .id(1)
-                    .nome("Notebook")
-                    .preco(new BigDecimal(5000))
-                    .quantidade(10)
-            .build(),
-            ProdutoEntity.builder()
-                    .id(2)
-                    .nome("Iphone")
-                    .preco(new BigDecimal(7000))
-                    .quantidade(10)
-            .build(),
-            ProdutoEntity.builder()
-                    .id(3)
-                    .nome("Mouse")
-                    .preco(new BigDecimal(500))
-                    .quantidade(10)
-            .build()
-    );
+    private static final List<ProdutoEntity> PRODUTOS = new ArrayList<>();
+
+            static {
+                PRODUTOS.add(ProdutoEntity.builder()
+                        .id(1)
+                        .nome("Notebook")
+                        .preco(new BigDecimal(5000))
+                        .quantidade(10)
+                        .build());
+
+                PRODUTOS.add(ProdutoEntity.builder()
+                                .id(2)
+                                .nome("Iphone")
+                                .preco(new BigDecimal(7000))
+                                .quantidade(10)
+                                .build());
+
+                PRODUTOS.add(ProdutoEntity.builder()
+                                .id(3)
+                                .nome("Mouse")
+                                .preco(new BigDecimal(500))
+                                .quantidade(10)
+                                .build());
+            };
 
     public List<ProdutoEntity> findAll() {
         return new ArrayList<>(PRODUTOS);
+    }
+
+    public ProdutoEntity createProduct(ProdutoDTO produtoDTO) {
+
+        Integer identificador = PRODUTOS.stream()
+                .mapToInt(ProdutoEntity::getId)
+                .max()
+                .orElse(0) + 1;
+
+        ProdutoEntity newProduto = ProdutoEntity.builder()
+                .id(identificador)
+                .nome(produtoDTO.getNome())
+                .preco(produtoDTO.getPreco())
+                .quantidade(produtoDTO.getQuantidade())
+                .build();
+
+        PRODUTOS.add(newProduto);
+
+        return newProduto;
     }
 }
