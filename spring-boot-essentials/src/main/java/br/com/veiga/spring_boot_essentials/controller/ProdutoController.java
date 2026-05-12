f2a36a235b9d13699c9b5120fedc6cb7408fcaa9
VeigaDevv <veigadevv@gmail.com>
2026-05-12 10:10:31 -0700

    feat: implement POST endpoint and constructor injection in ProdutoController

--- spring-boot-essentials/src/main/java/br/com/veiga/spring_boot_essentials/controller/ProdutoController.java
+++ spring-boot-essentials/src/main/java/br/com/veiga/spring_boot_essentials/controller/ProdutoController.java
@@ -1,14 +1,12 @@
 package br.com.veiga.spring_boot_essentials.controller;
 
 import br.com.veiga.spring_boot_essentials.database.model.ProdutoEntity;
+import br.com.veiga.spring_boot_essentials.dto.ProdutoDTO;
 import br.com.veiga.spring_boot_essentials.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,11 @@ public class ProdutoController {
     @ResponseStatus(HttpStatus.OK)
     public List<ProdutoEntity> findAll() {
         return produtoService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProdutoEntity create(@RequestBody ProdutoDTO produtoDTO) {
+        return produtoService.createProduct(produtoDTO);
     }
 }
